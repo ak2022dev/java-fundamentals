f3b091588785980b824ba081fecc8666348efbcb
Amir Khan <ak2022dev@gmail.com>
2024-03-14 11:47:04 +0000

    Add code to print args - just a reference at this stage

---
+++ bites/examples/ArgsPrint.java
@@ -0,0 +1,8 @@
+package bites.examples;
+
+public class ArgsPrint {
+    public static void main( String[] args ) {
+        System.out.println("The array of arguments passed to the program:" + args );
+        System.out.println("This prints the value of the reference to the array object, not the items in the array.");
+    }
+}
