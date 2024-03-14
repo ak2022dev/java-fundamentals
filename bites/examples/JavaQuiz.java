4933100539610fa35946677447c0dba7d8e5a316
Amir Khan <ak2022dev@gmail.com>
2024-03-14 11:28:51 +0000

    Add basic console output class

---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,10 @@
+package bites.examples;
+
+public class JavaQuiz {
+    public static void main( String[] args ) {
+        System.out.println("1. Yes, all Java code has to be inside a class");
+        System.out.println("2. Compilation in Java compiles the source-code into a lower-level byte-code. The byte-code is executed, not the source-code.");
+        System.out.println("3. The main method is the method at which execution begins. Without it, there is nothing to run. There needs to be exactly one main method.");
+        System.out.println("4. void means that the method does not return any value.");
+    }
+}
